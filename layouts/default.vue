e8b0d9c6a88ebf1aaa502b8c4faf6d54b9776a15
Zubairrafi <zubair@appscode.com>
2023-08-28 19:12:02 +0600

    layout added
    Signed-off-by: Zubairrafi <zubair@appscode.com>

---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+    <nav class="navbar">
+      <div class="nav-item">
+        <nuxt-link to="/">Home</nuxt-link>
+      </div>
+      <div class="nav-item">
+        <nuxt-link to="/mywork">My Work</nuxt-link>
+      </div>
+      <div class="nav-item">
+        <nuxt-link to="/posts">Post</nuxt-link>
+      </div>
+    </nav>
+
+    <div>
+        <slot />
+    </div>
+  </template>
+  
+  <style scoped>
+  .navbar {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background-color: #333;
+    color: white;
+    height: 60px;
+  }
+  
+  .nav-item {
+    padding: 10px;
+  }
+  .router-link-exact-active{
+    color: #12b488;
+  }
+  
+  /* Add more styling as needed */
+  </style>
+  
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,6 @@
 <template>
-  <nav class="navbar">
-    <div class="nav-item">
-      <nuxt-link to="/">Home</nuxt-link>
-    </div>
-    <div class="nav-item">
-      <nuxt-link to="/mywork">My Work</nuxt-link>
-    </div>
-    <div class="nav-item">
-      <nuxt-link to="/posts">Post</nuxt-link>
-    </div>
-  </nav>
+
+  This is Home page
 </template>
 
-<style scoped>
-.navbar {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  background-color: #333;
-  color: white;
-  height: 60px;
-}
 
-.nav-item {
-  padding: 10px;
-}
-
-/* Add more styling as needed */
-</style>
